26b7e95e0f665fe489688449a9949fde597cf823
Saifee56 <safuanalam123@gmail.com>
2024-03-12 12:03:14 +0600

    updated 12-03-24

--- credit_fraud/myapp/urls.py
+++ credit_fraud/myapp/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'myapp'
 urlpatterns = [
     path('',views.form1,name='form1'),
-    path('details',views.info,name='details'),
+    path('result',views.form_info,name='result'),
 ]
 
--- credit_fraud/myapp/views.py
+++ credit_fraud/myapp/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from joblib import load
+
+
+model=load(r'C:\Users\i-saifee\Desktop\Credit_card_fraud\Credit-Card-Fraud-Detection\credit_fraud/model.joblib')
+
 
 def form1(request):
     return render(request,'form1.html')
 
-#Form information
-def info(request):
-    transactionId = request.get('transactionId')
-    customerID = request.get('customerID')
-    terminalID = request.get('terminalID')
-    datetime = request.get('datetime')
-    transactionAmount = request.get('transactionAmount')
-    return render(request, 'details.html', {'details':{transactionId,transactionAmount,terminalID,customerID,datetime}})
+# #Form information
+def form_info(request):
+    merchant=request.GET['merchant']
+    category=request.get['category']
+    gender=request.GET['gender']
+    job=request.GET['job']
+    unix_time=request.GET['unix_time']
+    merch_lat=request.GET['merch_lat']
+    merch_long=request.GET['merch_long']
+
+    y_pred=model.predict([[merchant,category,gender,job,unix_time,merch_lat,merch_long]])
+
+    if y_pred[0] >0.5:
+        y_pred='The user is attempting fraud'
+    elif y_pred[0] <0.5:
+        y_pred='The user is pure'
+    
+    return render(request,result.html,{'result':y_pred})
+     
